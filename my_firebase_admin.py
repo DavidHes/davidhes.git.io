077ef9b6c8af36630d842d1a33a981a0403d9b8b
DavidHes <mcacczey@gmail.com>
2024-07-16 20:07:09 +0200

    Changed serviceAccountKey.json and database connection details

--- my_firebase_admin.py
+++ my_firebase_admin.py
@@ -1,6 +1,6 @@
 from firebase_admin import credentials, initialize_app, db
 
-cred = credentials.Certificate('serviceAccountKey.json')
+cred = credentials.Certificate('C:/Users/David/Documents/Studium/SoSe24/Entwicklung von Web-Anwendungen/wirklich final/davidhes.github.io/serviceAccountKey.json')
 initialize_app(cred, {
     'databaseURL': 'https://plate-saver-default-rtdb.europe-west1.firebasedatabase.app'
 })
